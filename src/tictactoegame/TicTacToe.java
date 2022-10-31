712ab2f9820be412ac13ad9b2a63ab90d5a39bef
hhmadan <hhmadan03@gmail.com>
2022-10-31 07:35:23 +0530

    ADDED: Ability of Computer to find its winning move,block user wins

--- src/tictactoegame/TicTacToe.java
+++ src/tictactoegame/TicTacToe.java
@@ -1,18 +1,19 @@
 package tictactoegame;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class TicTacToe {
-    static int x;
-    static char letter;
-    final static Scanner sc = new Scanner(System.in);
-    static int computerChoiceMove;
+
     static ArrayList<Integer> playerPos = new ArrayList<>();
     static ArrayList<Integer> computerPos = new ArrayList<>();
+    static char side;
+    static int blockPosition;
+    static int computerChoice;
+    final static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         char[][] ticTacToeBoard = {{' ', '|', ' ', '|', ' '},
@@ -21,68 +22,88 @@ public class TicTacToe {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}};
 
+        showBoard(ticTacToeBoard);
         int turnResult=whoseTurn();
-
-        while(true){
-            if(turnResult==1){              //Player's Turn
-                System.out.println("Its Your Turn..! Enter position to mark: ");
+        if(turnResult==1){
+            for(int i=1;i<=ticTacToeBoard.length;i++){
+                System.out.println("Enter Position between 1-9 ");
                 int playerChoice = sc.nextInt();
-                while (playerPos.contains(playerChoice) || computerPos.contains(playerChoice)){
-                    System.out.println("Position is Taken..! Enter free Cell");
+
+                while (playerPos.contains(playerChoice) || computerPos.contains(playerChoice)) {
+                    System.out.println("Position is taken..! Please Enter free cell position");
                     playerChoice = sc.nextInt();
                 }
-                playerMoves(playerChoice, letter,ticTacToeBoard);
-                //CHECK WIN TIE OR CHANGE TURN...
-                String output = checkStatus();
-                if(output.length()>0){
-                    System.out.println(output);
+                getMovesPlayer(side, playerChoice, ticTacToeBoard);
+                String status=checkStatus();
+                if(status.length()>0){
+                    System.out.println(status);
                     break;
                 }
-                System.out.println("Current Positions of Game Board is...");
+                //Displaying current Game Board Position
+                System.out.println("Current Game Board Position");
                 showBoard(ticTacToeBoard);
                 System.out.println("\n");
 
-                System.out.println("COMPUTER PLAYING.....");              //Computer's Turn
-                getComputerMoves(letter,ticTacToeBoard);
-            while (playerPos.contains(computerChoiceMove) || computerPos.contains(computerChoiceMove)){
-                getComputerMoves(letter,ticTacToeBoard);
-            }
-                System.out.println("Current Positions of Game Board is...");
-                showBoard(ticTacToeBoard);
-                output = checkStatus();
-                if(output.length()>0){
-                    System.out.println(output);
-                    break;
+                Random rd = new Random();
+                int computerChoice = rd.nextInt(9) + 1;
+                while (playerPos.contains(computerChoice) || computerPos.contains(computerChoice)) {
+                    //System.out.println("Position is taken..! Please Enter free cell position");
+                    computerChoice = rd.nextInt(9) + 1;
                 }
-            }
-            else{
-                System.out.println("COMPUTER PLAYING...");          //Computer's Turn
-                getComputerMoves(letter,ticTacToeBoard);
+                getMovesComputer(side, ticTacToeBoard);
 
-                while (playerPos.contains(computerChoiceMove) || computerPos.contains(computerChoiceMove)){
-                    getComputerMoves(letter,ticTacToeBoard);
-                }
-                System.out.println("Current Positions of Game Board is");
+                //Displaying current Game Board Position
+                System.out.println("Current Game Board Position");
                 showBoard(ticTacToeBoard);
-                String output = checkStatus();
-                if(output.length()>0){
-                    System.out.println(output);
+                System.out.println("\n");
+                status=checkStatus();
+                if(status.length()>0){
+                    System.out.println(status);
                     break;
                 }
-                System.out.println("Its Your Turn..! Enter position to mark: ");
+            }
+        }
+        else{
+            for(int i=1;i<=ticTacToeBoard.length;i++){
+                Random rd = new Random();
+                int computerChoice = rd.nextInt(9) + 1;
+                while (playerPos.contains(computerChoice) || computerPos.contains(computerChoice)) {
+                    computerChoice = rd.nextInt(9) + 1;
+                }
+                getMovesComputer(side, ticTacToeBoard);
+
+                //Displaying current Game Board Position
+                System.out.println("Current Game Board Position");
+                showBoard(ticTacToeBoard);
+                System.out.println("\n");
+                String status=checkStatus();
+                if(status.length()>0){
+                    System.out.println(status);
+                    break;
+                }
+                System.out.println("Enter Position between 1-9 ");
                 int playerChoice = sc.nextInt();
-                letter = 'O';
-                while (playerPos.contains(playerChoice) || computerPos.contains(playerChoice)){
-                    System.out.println("Position is Taken..! Enter free Cell");
+
+                while (playerPos.contains(playerChoice) || computerPos.contains(playerChoice)) {
+                    System.out.println("Position is taken..! Please Enter free cell position");
                     playerChoice = sc.nextInt();
                 }
-                playerMoves(playerChoice, letter,ticTacToeBoard);
+                side='O';
+                getMovesPlayer(side, playerChoice, ticTacToeBoard);
+                status=checkStatus();
+                if(status.length()>0){
+                    System.out.println(status);
+                    break;
+                }
+                //Displaying current Game Board Position
+                System.out.println("Current Game Board Position");
+                showBoard(ticTacToeBoard);
+                System.out.println("\n");
             }
-            System.out.println("Current Positions of Game Board is...");
-            showBoard(ticTacToeBoard);
         }
-        }
-    public static void showBoard (char[][] ticTacToeBoard){
+    }
+
+    public static void showBoard ( char[][] ticTacToeBoard){
         for (char[] row : ticTacToeBoard) {
             for (char column : row) {
                 System.out.print(column);
@@ -90,19 +111,20 @@ public class TicTacToe {
             System.out.println();
         }
     }
+
     public static int whoseTurn(){
         Random random = new Random();
-        x = random.nextInt(2)+1;
+        int x = random.nextInt(2)+1;
         if (x==1){
             System.out.println("Its You to Play First..!");
             System.out.println("What you want==> X or O? Enter..");
-            letter = sc.next().charAt(0);
+            side = sc.next().charAt(0);
         }
         else
             System.out.println("Its Computer to Play First..!");
         return x;
     }
-    public static void playerMoves(int playerChoice, char letter, char[][]ticTacToeBoard){
+    public static void getMovesPlayer ( char letter, int playerChoice, char[][] ticTacToeBoard){
         playerPos.add(playerChoice);
         switch (playerChoice) {
             case 1:
@@ -136,48 +158,174 @@ public class TicTacToe {
                 break;
         }
     }
-    public static void getComputerMoves(char letter, char[][]ticTacToeBoard){
+    public static void getMovesComputer ( char letter, char[][] ticTacToeBoard) {
         char side = ' ';
         if (letter == 'X' || letter == 'x')
             side = 'O';
         else
             side = 'X';
 
-        Random rd = new Random();
-        int ch = rd.nextInt(9)+1;
-        switch (ch) {
-            case 1:
-                ticTacToeBoard[0][0] = side;
-                break;
-            case 2:
-                ticTacToeBoard[0][2] = side;
-                break;
-            case 3:
-                ticTacToeBoard[0][4] = side;
-                break;
-            case 4:
-                ticTacToeBoard[2][0] = side;
-                break;
-            case 5:
-                ticTacToeBoard[2][2] = side;
-                break;
-            case 6:
-                ticTacToeBoard[2][4] = side;
-                break;
-            case 7:
-                ticTacToeBoard[4][0] = side;
-                break;
-            case 8:
-                ticTacToeBoard[4][2] = side;
-                break;
-            case 9:
-                ticTacToeBoard[4][4] = side;
-                break;
-            default:
-                break;
+        int winPosCompResult = normalWinPositionOfComputer(ticTacToeBoard,side);
+        if(winPosCompResult>0){
+            computerChoice = winPosCompResult;
+            computerPos.add(computerChoice);
         }
+        int blockMove = blockPlayer(ticTacToeBoard,letter,side);
+        if(blockMove>0){
+            computerChoice = blockPosition;
+        }
+        else{
+            Random rd = new Random();
+            computerChoice = rd.nextInt(9)+1;
+            switch (computerChoice) {
+                case 1:
+                    ticTacToeBoard[0][0] = side;
+                    break;
+                case 2:
+                    ticTacToeBoard[0][2] = side;
+                    break;
+                case 3:
+                    ticTacToeBoard[0][4] = side;
+                    break;
+                case 4:
+                    ticTacToeBoard[2][0] = side;
+                    break;
+                case 5:
+                    ticTacToeBoard[2][2] = side;
+                    break;
+                case 6:
+                    ticTacToeBoard[2][4] = side;
+                    break;
+                case 7:
+                    ticTacToeBoard[4][0] = side;
+                    break;
+                case 8:
+                    ticTacToeBoard[4][2] = side;
+                    break;
+                case 9:
+                    ticTacToeBoard[4][4] = side;
+                    break;
+                default:
+                    break;
+            }
+        }
+        computerPos.add(computerChoice);
     }
-
+    public static int normalWinPositionOfComputer(char[][]ticTacToeBoard, char side){
+        int winPosOfComp;
+        if ((ticTacToeBoard[0][2] == side && ticTacToeBoard[0][4] == side && ticTacToeBoard[0][0] == ' ') || (ticTacToeBoard[2][0] == side && ticTacToeBoard[4][0] == side && ticTacToeBoard[0][0] == ' ') || (ticTacToeBoard[2][2] == side && ticTacToeBoard[4][4] == side && ticTacToeBoard[0][0] == ' '))
+        {
+            ticTacToeBoard[0][0]=side;
+            winPosOfComp = 1;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[0][0] == side && ticTacToeBoard[0][4] == side && ticTacToeBoard[0][2] == ' ') || (ticTacToeBoard[2][2] == side && ticTacToeBoard[4][2] == side && ticTacToeBoard[0][2] == ' '))
+        {
+            ticTacToeBoard[0][2] = side;
+            winPosOfComp = 2;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[0][0] == side && ticTacToeBoard[0][2] == side && ticTacToeBoard[0][4] == ' ') || (ticTacToeBoard[4][0] == side && ticTacToeBoard[2][2] == side && ticTacToeBoard[0][4] == ' ') || (ticTacToeBoard[2][4] == side && ticTacToeBoard[4][4] == side && ticTacToeBoard[0][4] == ' '))
+        {
+            ticTacToeBoard[0][4] = side;
+            winPosOfComp = 3;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[0][0] == side && ticTacToeBoard[4][0] == side && ticTacToeBoard[2][0] == ' ') || (ticTacToeBoard[2][2] == side && ticTacToeBoard[2][4] == side && ticTacToeBoard[2][0] == ' '))
+        {
+            ticTacToeBoard[2][0] = side;
+            winPosOfComp = 4;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[0][2] == side && ticTacToeBoard[4][2] == side && ticTacToeBoard[2][2] == ' ') || (ticTacToeBoard[2][0] == side && ticTacToeBoard[2][4] == side && ticTacToeBoard[2][2] == ' ')
+                || (ticTacToeBoard[0][0] == side && ticTacToeBoard[4][4]== side && ticTacToeBoard[2][2] == ' ') || (ticTacToeBoard[4][0] == side && ticTacToeBoard[0][4] == side && ticTacToeBoard[2][2] == ' '))
+        {
+            ticTacToeBoard[2][2] = side;
+            winPosOfComp = 5;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[0][4] == side && ticTacToeBoard[4][4] == side && ticTacToeBoard[2][4] == ' ') || (ticTacToeBoard[2][0] == side && ticTacToeBoard[2][2] == side && ticTacToeBoard[2][4] == ' '))
+        {
+            winPosOfComp = 6;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[0][0] == side && ticTacToeBoard[2][0] == side && ticTacToeBoard[4][0] == ' ') || (ticTacToeBoard[4][2] == side && ticTacToeBoard[4][4] == side && ticTacToeBoard[4][0] == ' ') || (ticTacToeBoard[2][2] == side && ticTacToeBoard[0][4] == side && ticTacToeBoard[4][0] == ' '))
+        {
+            ticTacToeBoard[4][0] = side;
+            winPosOfComp = 7;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[4][0] == side && ticTacToeBoard[4][4] == side && ticTacToeBoard[4][2] == ' ') || (ticTacToeBoard[0][2] == side && ticTacToeBoard[2][2] == side && ticTacToeBoard[4][2] == ' '))
+        {
+            ticTacToeBoard[4][2] =side;
+            winPosOfComp = 8;
+            return winPosOfComp;
+        }
+        else if ((ticTacToeBoard[0][0] == side && ticTacToeBoard[2][2] == side && ticTacToeBoard[4][4] == ' ') || (ticTacToeBoard[4][0] == side && ticTacToeBoard[4][2] == side && ticTacToeBoard[4][4] == ' ') || (ticTacToeBoard[2][4] == side && ticTacToeBoard[0][4] == side && ticTacToeBoard[4][4] == ' '))
+        {
+            ticTacToeBoard[4][4] =side;
+            winPosOfComp = 9;
+            return winPosOfComp;
+        }
+        return 0;
+    }
+    public static int blockPlayer(char[][]ticTacToeBoard, char letter, char side){
+        if ((ticTacToeBoard[0][2] == letter && ticTacToeBoard[0][4] == letter && ticTacToeBoard[0][0] == ' ') || (ticTacToeBoard[2][0] == letter && ticTacToeBoard[4][0] == letter && ticTacToeBoard[0][0] == ' ') || (ticTacToeBoard[2][2] == letter && ticTacToeBoard[4][4] == letter && ticTacToeBoard[0][0] == ' '))
+        {
+            ticTacToeBoard[0][0]=side;
+            blockPosition = 1;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[0][0] == letter && ticTacToeBoard[0][4] == letter && ticTacToeBoard[0][2] == ' ') || (ticTacToeBoard[2][2] == letter && ticTacToeBoard[4][2] == letter && ticTacToeBoard[0][2] == ' '))
+        {
+            ticTacToeBoard[0][2] = side;
+            blockPosition = 2;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[0][0] == letter && ticTacToeBoard[0][2] == letter && ticTacToeBoard[0][4] == ' ') || (ticTacToeBoard[4][0] == letter && ticTacToeBoard[2][2] == letter && ticTacToeBoard[0][4] == ' ') || (ticTacToeBoard[2][4] == letter && ticTacToeBoard[4][4] == letter && ticTacToeBoard[0][4] == ' '))
+        {
+            ticTacToeBoard[0][4] = side;
+            blockPosition = 3;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[0][0] == letter && ticTacToeBoard[4][0] == letter && ticTacToeBoard[2][0] == ' ') || (ticTacToeBoard[2][2] == letter && ticTacToeBoard[2][4] == letter && ticTacToeBoard[2][0] == ' '))
+        {
+            ticTacToeBoard[2][0] = side;
+            blockPosition = 4;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[0][2] == letter && ticTacToeBoard[4][2] == letter && ticTacToeBoard[2][2] == ' ') || (ticTacToeBoard[2][0] == letter && ticTacToeBoard[2][4] == letter && ticTacToeBoard[2][2] == ' ')
+                || (ticTacToeBoard[0][0] == letter && ticTacToeBoard[4][4]== letter && ticTacToeBoard[2][2] == ' ') || (ticTacToeBoard[4][0] == letter && ticTacToeBoard[0][4] == letter && ticTacToeBoard[2][2] == ' '))
+        {
+            ticTacToeBoard[2][2] = side;
+            blockPosition = 5;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[0][4] == letter && ticTacToeBoard[4][4] == letter && ticTacToeBoard[2][4] == ' ') || (ticTacToeBoard[2][0] == letter && ticTacToeBoard[2][2] == letter && ticTacToeBoard[2][4] == ' '))
+        {
+            blockPosition = 6;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[0][0] == letter && ticTacToeBoard[2][0] == letter && ticTacToeBoard[4][0] == ' ') || (ticTacToeBoard[4][2] == letter && ticTacToeBoard[4][4] == letter && ticTacToeBoard[4][0] == ' ') || (ticTacToeBoard[2][2] == letter && ticTacToeBoard[0][4] == letter && ticTacToeBoard[4][0] == ' '))
+        {
+            ticTacToeBoard[4][0] = side;
+            blockPosition = 7;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[4][0] == letter && ticTacToeBoard[4][4] == letter && ticTacToeBoard[4][2] == ' ') || (ticTacToeBoard[0][2] == letter && ticTacToeBoard[2][2] == letter && ticTacToeBoard[4][2] == ' '))
+        {
+            ticTacToeBoard[4][2] =side;
+            blockPosition = 8;
+            return blockPosition;
+        }
+        else if ((ticTacToeBoard[0][0] == letter && ticTacToeBoard[2][2] == letter && ticTacToeBoard[4][4] == ' ') || (ticTacToeBoard[4][0] == letter && ticTacToeBoard[4][2] == letter && ticTacToeBoard[4][4] == ' ') || (ticTacToeBoard[2][4] == letter && ticTacToeBoard[0][4] == letter && ticTacToeBoard[4][4] == ' '))
+        {
+            ticTacToeBoard[4][4] =side;
+            blockPosition = 9;
+            return blockPosition;
+        }
+        return 0;
+    }
     public static String checkStatus(){
         List firstRow = Arrays.asList(1, 2, 3);
         List midRow = Arrays.asList(4, 5, 6);
@@ -202,11 +350,14 @@ public class TicTacToe {
             if (playerPos.containsAll(list)) {
                 return "CONGRATULATIONS..! YOU WON";
             } else if (computerPos.containsAll(list)) {
-                return "OOPS..! COMPUTER WON";
-            } else if (playerPos.size() + computerPos.size() == 9) {
-                return "ITS TIE..!";
+                return "COMPUTER WON..!";
+            } else if (computerPos.size()+playerPos.size()==9) {
+                return "ITS A TIE..!";
             }
         }
         return "";
     }
+
 }
+
+
