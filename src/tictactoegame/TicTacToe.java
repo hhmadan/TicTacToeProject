8a20567aed1498cdd90c4a4c61718c4b020d0909
hhmadan <hhmadan03@gmail.com>
2022-10-31 07:10:57 +0530

    ADDED: Toss to check who will Play First

--- src/tictactoegame/TicTacToe.java
+++ src/tictactoegame/TicTacToe.java
@@ -1,14 +1,18 @@
 package tictactoegame;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
-
+import java.util.Random;
+import java.util.List;
+import java.util.Arrays;
 
 public class TicTacToe {
+    static int x;
+    static char letter;
+    final static Scanner sc = new Scanner(System.in);
+    static int computerChoiceMove;
     static ArrayList<Integer> playerPos = new ArrayList<>();
     static ArrayList<Integer> computerPos = new ArrayList<>();
-    final static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         char[][] ticTacToeBoard = {{' ', '|', ' ', '|', ' '},
@@ -17,44 +21,55 @@ public class TicTacToe {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}};
 
-        showBoard(ticTacToeBoard);
-        System.out.println("What you want==> X or O? Enter..");
-        char letter = sc.next().charAt(0);
+        int turnResult=whoseTurn();
 
-        for(int i=1;i<ticTacToeBoard.length;i++){
-            System.out.println("Enter Position between 1-9 ");
-            int playerChoice = sc.nextInt();
+        while(true){
+            if(turnResult==1){              //Player's Turn
+                System.out.println("Its Your Turn..! Enter position to mark: ");
+                int playerChoice = sc.nextInt();
+                while (playerPos.contains(playerChoice) || computerPos.contains(playerChoice)){
+                    System.out.println("Position is Taken..! Enter free Cell");
+                    playerChoice = sc.nextInt();
+                }
+                playerMoves(playerChoice, letter,ticTacToeBoard);
+                //CHECK WIN TIE OR CHANGE TURN...
 
-            while (playerPos.contains(playerChoice) || computerPos.contains(playerChoice)) {
-                System.out.println("Position is taken..! Please Enter free cell position");
-                playerChoice = sc.nextInt();
+                System.out.println("Current Positions of Game Board is...");
+                showBoard(ticTacToeBoard);
+                System.out.println("\n");
+
+                System.out.println("COMPUTER PLAYING.....");              //Computer's Turn
+                getComputerMoves(letter,ticTacToeBoard);
+            while (playerPos.contains(computerChoiceMove) || computerPos.contains(computerChoiceMove)){
+                getComputerMoves(letter,ticTacToeBoard);
             }
-            getMovesPlayer(letter, playerChoice, ticTacToeBoard);
-
-            //Displaying current Game Board Position
-            System.out.println("Current Game Board Position");
-            showBoard(ticTacToeBoard);
-            System.out.println("\n");
-
-            Random rd = new Random();
-            int computerChoice = rd.nextInt(9) + 1;
-            while (playerPos.contains(computerChoice) || computerPos.contains(computerChoice)) {
-                System.out.println("Position is taken..! Please Enter free cell position");
-                computerChoice = rd.nextInt(9) + 1;
+                System.out.println("Current Positions of Game Board is...");
+                showBoard(ticTacToeBoard);
             }
-            getMovesComputer(letter, computerChoice, ticTacToeBoard);
+            else{
+                System.out.println("COMPUTER PLAYING...");          //Computer's Turn
+                getComputerMoves(letter,ticTacToeBoard);
 
-            //Displaying current Game Board Position
-            System.out.println("Current Game Board Position");
+                while (playerPos.contains(computerChoiceMove) || computerPos.contains(computerChoiceMove)){
+                    getComputerMoves(letter,ticTacToeBoard);
+                }
+                System.out.println("Current Positions of Game Board is");
+                showBoard(ticTacToeBoard);
+
+                System.out.println("Its Your Turn..! Enter position to mark: ");
+                int playerChoice = sc.nextInt();
+                letter = 'O';
+                while (playerPos.contains(playerChoice) || computerPos.contains(playerChoice)){
+                    System.out.println("Position is Taken..! Enter free Cell");
+                    playerChoice = sc.nextInt();
+                }
+                playerMoves(playerChoice, letter,ticTacToeBoard);
+            }
+            System.out.println("Current Positions of Game Board is...");
             showBoard(ticTacToeBoard);
-            System.out.println("\n");
-
-            // isFull(ticTacToeBoard);
         }
-        System.out.println("Game Board is Full..!");
     }
-
-    public static void showBoard ( char[][] ticTacToeBoard){
+    public static void showBoard (char[][] ticTacToeBoard){
         for (char[] row : ticTacToeBoard) {
             for (char column : row) {
                 System.out.print(column);
@@ -62,7 +77,19 @@ public class TicTacToe {
             System.out.println();
         }
     }
-    public static void getMovesPlayer ( char letter, int playerChoice, char[][] ticTacToeBoard){
+    public static int whoseTurn(){
+        Random random = new Random();
+        x = random.nextInt(2)+1;
+        if (x==1){
+            System.out.println("Its You to Play First..!");
+            System.out.println("What you want==> X or O? Enter..");
+            letter = sc.next().charAt(0);
+        }
+        else
+            System.out.println("Its Computer to Play First..!");
+        return x;
+    }
+    public static void playerMoves(int playerChoice, char letter, char[][]ticTacToeBoard){
         playerPos.add(playerChoice);
         switch (playerChoice) {
             case 1:
@@ -96,14 +123,16 @@ public class TicTacToe {
                 break;
         }
     }
-    public static void getMovesComputer ( char letter, int computerChoice, char[][] ticTacToeBoard){
-        computerPos.add(computerChoice);
+    public static void getComputerMoves(char letter, char[][]ticTacToeBoard){
         char side = ' ';
         if (letter == 'X' || letter == 'x')
             side = 'O';
         else
             side = 'X';
-        switch (computerChoice) {
+
+        Random rd = new Random();
+        int ch = rd.nextInt(9)+1;
+        switch (ch) {
             case 1:
                 ticTacToeBoard[0][0] = side;
                 break;
